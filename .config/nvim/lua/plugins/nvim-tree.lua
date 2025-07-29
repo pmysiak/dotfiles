7b8b349752c5b9f66beeb19be8b7cae85b884ec0
Peter Myšiak <peter.mysiak@gmail.com>
2025-07-30 00:11:08 +0200

    Update.

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
-local function my_on_attach(bufnr)
-    local api = require "nvim-tree.api"
 
+local function my_on_attach(bufnr)
+    local api = require("nvim-tree.api")
     local function opts(desc)
         return {
             desc = "nvim-tree: " .. desc,
@@ -37,10 +37,45 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         config = function()
+            local api = require("nvim-tree.api")
+
+            -- Automatically open file upon creation
+            api.events.subscribe(
+                api.events.Event.FileCreated,
+                function(file)
+                    vim.cmd("edit " .. vim.fn.fnameescape(file.fname))
+                end
+            )
+
             require("nvim-tree").setup({
                 on_attach = my_on_attach,
                 renderer = {
                     full_name = true,
+                    highlight_modified = "all",
+                    highlight_git = "all",
+                    icons = {
+                        show = {
+                            git = false,
+                            modified = false,
+                        },
+                    },
+                },
+                git = {
+                    enable = true,
+                },
+                modified = {
+                    enable = true,
+                },
+                filters = {
+                    enable = false,
+                },
+                notify = {
+                    threshold = vim.log.levels.WARN,
+                },
+                ui = {
+                    confirm = {
+                        default_yes = true,
+                    },
                 },
                 view = {
                     width = 50,
@@ -59,4 +94,3 @@ return {
         },
     },
 }
-
